b3033b2cfb4c3eaeaec349d63bb04edf6ba5521e
Hemanth Kumar <96412405+hemanthkumar316@users.noreply.github.com>
2026-02-20 00:14:50 +0530

    formated

--- src/UserList.js
+++ src/UserList.js
@@ -7,35 +7,31 @@ const UserList = () => {
     fetchUsers();
   }, []);
   const fetchUsers = async () => {
-      setLoading(true);
-      try{
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users",
-    );
-    console.log("users data", response.data);
-    setUsers(response.data);
-      }
-      catch(err){
-        console.log('Err',err)
-      } finally{
-          setLoading(false);
-      }
-
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+      console.log("users data", response.data);
+      setUsers(response.data);
+    } catch (err) {
+      console.log("Err", err);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div>
-        {loading && <p>Loading...</p>}
-      { !loading && users.length > 0 ? (
-        users.map((user, index) => {
-          return (
-            <div key={index}>
-              <p>{user.name}</p>
-            </div>
-          );
-        })
-      ) : (
-       !loading && <p>No users found</p>
-      )}
+      {loading && <p>Loading...</p>}
+      {!loading && users.length > 0
+        ? users.map((user, index) => {
+            return (
+              <div key={index}>
+                <p>{user.name}</p>
+              </div>
+            );
+          })
+        : !loading && <p>No users found</p>}
     </div>
   );
 };
